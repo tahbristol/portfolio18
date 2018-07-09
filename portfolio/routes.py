1882c539fffc663ce5f35e7e4e61eac6ce629b9d
Tyler Bristol <tbristol@thesoftedge.com>
2018-07-08 23:59:46 -0400

    handle form input

--- portfolio/routes.py
+++ portfolio/routes.py
@@ -1,6 +1,15 @@
-from flask import render_template
+from flask import render_template, request
 from portfolio import app
 	
 @app.route('/')
 def index():
 	return render_template('index.html')
+	
+@app.route('/contact', methods=['POST'])
+def contact():
+	req_data = request.form
+	name = req_data['name']
+	email = req_data['email']
+	phone = req_data['phone']
+	message = req_data['message']
+	#add email sending logic

5233a60459569fcc5f5dcf7c92ce06e516c5103f
tahbristol <tahbristol@gmail.com>
2018-07-10 22:15:34 -0400

    contact form functionality

--- portfolio/routes.py
+++ portfolio/routes.py
@@ -1,6 +1,31 @@
-from flask import render_template
+from smtplib import SMTP
+from flask import render_template, request
 from portfolio import app
 	
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/email', methods=['POST'])
+def email():
+	form = request.form
+	msg = {}
+	msg['To'] = "tahbristol@gmail.com"
+	msg['Subject'] = "From tahbristol.com"
+	msg['Name'] = form['name']
+	msg['From'] = form['email']
+	msg['Phone'] = form['phone']
+	msg['Content'] = form['message']
+
+	content = msg['From'] + "\n" + "Phone: " + msg['Phone'] + "\n" + "Message: " + msg['Content'] + "\n" + "Name: " + msg['Name']
+	
+	s = SMTP('smtp.gmail.com', 587)
+	s.ehlo()
+	s.starttls()
+	s.login('tahbristol@gmail.com', 'xuxbqluoqupbarro')
+	
+	s.sendmail('tahbristol@gmail.com', 'tahbristol@gmail.com', content)
+	s.close()
+	return render_template('index.html')
+	
+	
